0d5ca77433dca7443927aca7cc9a7b67ed7aab0c
Прохницкий Вадим Владиславович <vadimlinkin@gmail.com>
2024-04-13 19:20:39 +0500

    delete Automation1

---
+++ Automation1/HomeWork_Function.py
@@ -0,0 +1,9 @@
+# Issue_1
+def dergee_converter_to_fahrenheit(num):
+    fahrenheit_degree = num * 9/5 + 32
+    return fahrenheit_degree
+
+celsius_degree = int(input())
+
+print(dergee_converter_to_fahrenheit(celsius_degree))
+
---
+++ Automation1/HomeWork_Variable.py
@@ -0,0 +1,26 @@
+# Issue_1
+# def dergee_converter(num):
+#     fahrenheit_degree = num * 9/5 + 32
+#     return fahrenheit_degree
+
+# celsius_degree = int(input())
+
+# print(dergee_converter(celsius_degree))
+
+
+# Issue_2
+# def average(num):
+#     avg = sum(num) / len(num)
+#     return int(avg)
+
+# numbers = map(int, input("Введите 3 числа: ").split())
+
+# print(average(numbers))
+
+# Issue_3
+# def is_even(num):
+#     return ("Число чётное") if num % 2 == 0 else ("Число не чётное")
+
+# num = int(input("Введите число: "))
+
+# print(is_even(num))
